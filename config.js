3b9816f2dbeb4218a02f8e62a69af42d7e0905ea
Cjano00 <135664794+Cjano00@users.noreply.github.com>
2023-06-06 16:51:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'https://www.el-tiempo.net/api/json/v2/municipios', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '39.986038',
+	defaultLongitude: '-0.036070',
 
 	// Autochange
 	autoChangeTheme: true,
